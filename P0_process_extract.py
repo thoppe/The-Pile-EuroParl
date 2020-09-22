b5b118369921cc73cdb3bf53235f86cb1d9fc011
Travis Hoppe <travis.hoppe@gmail.com>
2020-09-22 14:10:42 -0400

    Add README

--- P0_process_extract.py
+++ P0_process_extract.py
@@ -3,36 +3,37 @@ from dspipe import Pipe
 import bs4
 import jsonlines
 
-f_save = 'EuroParliamentProceedings_1996_2011.jsonl'
+f_save = "EuroParliamentProceedings_1996_2011.jsonl"
 
 def process_tag(original_tag):
-    tag = original_tag.strip('>').strip('<')
+    tag = original_tag.strip(">").strip("<")
 
     # Skip empty tags
     if not tag:
         return None
-    
+
     tagname = tag.split()[0]
 
     # Skip paragraph, break, and chapter tags
-    if tagname in ['P', 'BRK', 'CHAPTER', '/P']:
+    if tagname in ["P", "BRK", "CHAPTER", "/P"]:
         return None
 
     # For speaker tags, return the name
-    if tagname == 'SPEAKER':
-        soup = bs4.BeautifulSoup(original_tag, 'html.parser')
-        name = soup.speaker['name']
+    if tagname == "SPEAKER":
+        soup = bs4.BeautifulSoup(original_tag, "html.parser")
+        name = soup.speaker["name"]
         return name
 
     # Raise a error here if there is a tag we don't know
     raise ValueError(f"Unknown tag {tag}")
-    
+
+
 def compute(f0):
-    
-    language = str(f0.parent).split('/')[-1]
-    
+
+    language = str(f0.parent).split("/")[-1]
+
     try:
-        with open(f0, encoding='utf-8') as FIN:
+        with open(f0, encoding="utf-8") as FIN:
             text = FIN.read()
     except:
         print(f"Failed to open {f0}")
@@ -44,33 +45,26 @@ def compute(f0):
 
     parsed_text = []
 
-    for line in text.split('\n'):
+    for line in text.split("\n"):
         if line and line[0] == "<" and line[-1] == ">":
             line = process_tag(line)
 
             # Skip lines that we want to filter
             if line is None:
                 continue
-            
+
         parsed_text.append(line)
 
-    text = '\n'.join(parsed_text)
+    text = "\n".join(parsed_text)
 
-    data = {
-        "text" : text,
-        "meta" : {
-            "language" : language
-        }
-    }
+    data = {"text": text, "meta": {"language": language}}
 
     return data
-    
-        
+
+
 source = Path("txt").glob("**/*.txt")
 P = Pipe(source, prefilter=False)(compute, -1)
 
-with jsonlines.open(f_save, 'w') as FOUT:
+with jsonlines.open(f_save, "w") as FOUT:
     for row in P:
         FOUT.write(row)
-
-
---
+++ README.md
@@ -0,0 +1,34 @@
+# The-Pile-EuroParl
+
+Notes: Failed to open txt/pl/ep-09-10-22-009.txt
+
+Parse, and filter data from . Data-ready for The-Pile.
+Download, parse, and filter the [European Parliament Proceedings](http://www.statmt.org/europarl/), data-ready for [The-Pile](https://github.com/EleutherAI/The-Pile).
+
+The Europarl parallel corpus is extracted from the proceedings of the European Parliament. It includes versions in 21 European languages: Romanic (French, Italian, Spanish, Portuguese, Romanian), Germanic (English, Dutch, German, Danish, Swedish), Slavik (Bulgarian, Czech, Polish, Slovak, Slovene), Finni-Ugric (Finnish, Hungarian, Estonian), Baltic (Latvian, Lithuanian), and Greek.
+
+To use this parser, first download the source file
+
+http://www.statmt.org/europarl/v7/europarl.tgz
+
+and unpack it to the directory. The parser will look for all file within the `txt` subdirectory. Note that the download is slow and make take 12 or more hours.
+
+The parser removes all basic tag information and only retains the name. The tag
+
+    <SPEAKER ID=77 LANGUAGE="NL" NAME="Pronk">
+
+Is reduced to
+
+    Pronk
+
+Extremely short files (<200 chracters) are removed as they did not contain useful language modeling text. No other filtering was done. 
+
+     ✔ Saved to EuroParliamentProceedings_1996_2011.jsonl
+     ℹ Saved 187,072 articles
+     ℹ Uncompressed filesize   4,941,430,389
+     ℹ Compressed filesize     1,475,803,930
+
+Data souce temporary hosted at 
+
+     > sha256sum EuroParliamentProceedings
+    dfc7cd52beba0eb03cbc0498993983f9a5d03da017bcc8f74257be942bf41877  EuroParliamentProceedings_1996_2011.jsonl.zst
