cd7e13a6b3d451732cb8b1245596703c801d5983
Travis Hoppe <travis.hoppe@gmail.com>
2020-09-23 11:00:26 -0400

    Add info box

---
+++ C0_process_infofile.py
@@ -0,0 +1,151 @@
+import jsonlines
+import json
+from tqdm import tqdm
+from pathlib import Path
+import hashlib
+import datetime
+
+"""
+Update these lines for the dataset meta information
+"""
+info = {
+    "title": "European Parliament Proceedings Parallel Corpus",
+    "url": "http://www.statmt.org/europarl/",
+    "min_year": 1996,
+    "max_year": 2011,
+}
+
+f_dataset = "EuroParliamentProceedings_1996_2011.jsonl"
+f_compressed = "EuroParliamentProceedings_1996_2011.jsonl.zst"
+
+# This is the specific line to show the sample, adjust for a good example
+sample_row_n = 20003
+
+
+def compute_hash(f, blocksize=4096):
+    print(f"Computing hash for {f}")
+
+    sha256_hash = hashlib.sha256()
+    with open(f, "rb") as FIN:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: FIN.read(blocksize), b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+
+def measure_file(f):
+    f = Path(f)
+    assert f.exists()
+
+    return {
+        "filename": str(f),
+        "size": f.stat().st_size,
+        "last_modified_UTC": int(f.stat().st_mtime),
+        "sha256_hash": compute_hash(f),
+    }
+
+
+# Create an infofile if it doesn't exist, otherwise read prior values
+save_dest = Path("doc")
+save_dest.mkdir(exist_ok=True)
+
+f_info = save_dest / "dataset_information.json"
+f_markdown = save_dest / (Path(f_dataset).name + '.md')
+
+if Path(f_info).exists():
+    with open(f_info) as FIN:
+        info.update(json.load(FIN))
+
+if "file_information" not in info:
+    info["file_information"] = {
+        "uncompressed": measure_file(f_dataset),
+        "compressed": measure_file(f_compressed),
+    }
+
+
+if "corpus_statistics" not in info:
+    print(f"Computing statistics for {f_dataset}")
+
+    stats = {
+        "total_documents": 0,
+        "total_character_count": 0,
+        "total_word_count": 0,
+        "max_character_count": 0,
+        "min_character_count": 10 ** 20,
+        "max_word_count": 0,
+        "min_word_count": 10 ** 20,
+    }
+
+    sample = {}
+
+    with jsonlines.open(f_dataset) as FIN:
+        for n, row in enumerate(tqdm(FIN)):
+            text = row["text"]
+            words = text.split()
+
+            stats["total_documents"] += 1
+            stats["total_character_count"] += len(text)
+            stats["total_word_count"] += len(words)
+
+            stats["max_character_count"] = max(len(text), stats["max_character_count"])
+            stats["min_character_count"] = min(len(text), stats["min_character_count"])
+
+            stats["max_word_count"] = max(len(words), stats["max_word_count"])
+            stats["min_word_count"] = min(len(words), stats["min_word_count"])
+
+            if n == sample_row_n:
+                sample["text"] = row["text"]
+                sample["meta"] = row["meta"]
+                sample["sample_row_n"] = sample_row_n
+
+                print(text)
+
+    info["corpus_statistics"] = stats
+    info["sample_data"] = sample
+
+
+js = json.dumps(info, indent=2)
+
+meta_pp = json.dumps(info["sample_data"]["meta"], indent=2)
+
+with open(f_info, "w") as FOUT:
+    FOUT.write(js)
+
+print(js)
+
+# Now build a nice markdown version of the datafile
+
+MD = f'''
+# {info['title']}
+
+| Key             | Value |
+| ---             | ---:   |
+| Uncompressed file size | {info["file_information"]["uncompressed"]["size"]:,} |
+| Year range | {info["min_year"]} - {info["max_year"]} |
+| Document count  | {info["corpus_statistics"]["total_documents"]:,} |
+| Word count      | {info["corpus_statistics"]["total_word_count"]:,} |
+| Character count | {info["corpus_statistics"]["total_character_count"]:,} |
+
+### Sample Text (found at row {sample_row_n})
+
+```
+{info["sample_data"]["text"]}
+```
+
+### Sample meta information
+
+```json
+{meta_pp}
+```
+
+### Checksums, sha256sum
+
+| Filename             | Checksum |
+| ---             | :---   |
+| Compressed | `{info["file_information"]["uncompressed"]["sha256_hash"]}` |
+| Uncompressed | `{info["file_information"]["uncompressed"]["sha256_hash"]}` |
+'''
+
+with open(f_markdown, 'w') as FOUT:
+    FOUT.write(MD)
+
---
+++ doc/EuroParliamentProceedings_1996_2011.jsonl.md
@@ -0,0 +1,42 @@
+
+# European Parliament Proceedings Parallel Corpus
+
+| Key             | Value |
+| ---             | ---:   |
+| Uncompressed file size | 4,940,726,841 |
+| Year range | 1996 - 2011 |
+| Document count  | 69,814 |
+| Word count      | 670,657,534 |
+| Character count | 4,524,334,009 |
+
+### Sample Text (found at row 20003)
+
+```
+Opening of the sitting
+(The sitting was opened at 9 a.m.)
+Daniel Cohn-Bendit
+- (FR) Madam President, I would like to check something with you. Apparently it was decided, at the request of the French Presidency, to control photography during President Sarkozy's speech.
+I would like to know who took this decision, who requested it, and who is in charge of photography at the European Parliament. Is it the European Parliament or the French Presidency? I would appreciate an answer before the start of the sitting.
+President
+Mr Cohn-Bendit, you will have an answer before the start of the sitting, but for now, if you do not mind, we will proceed with the voting.
+Astrid Lulling
+- (FR) Madam President, as Quaestor of this Parliament, I would like to draw your attention to the fact that the route to the Parliament entrance is blocked on one side by riot police. They are not allowing official cars to enter. If some Members arrive late because they have to make another detour, they should not be penalised if they cannot take part in the voting.
+President
+Mrs Lulling, clearly this will be taken into account, because some Members are having difficulty in being admitted to Parliament.
+
+```
+
+### Sample meta information
+
+```json
+{
+  "language": "en"
+}
+```
+
+### Checksums, sha256sum
+
+| Filename             | Checksum |
+| ---             | :---   |
+| Compressed | `fbeb84ec7c1174597e3bebfdddce7f8aa5ecc5d1ab1a57fafd020368e9b6c6e4` |
+| Uncompressed | `fbeb84ec7c1174597e3bebfdddce7f8aa5ecc5d1ab1a57fafd020368e9b6c6e4` |
